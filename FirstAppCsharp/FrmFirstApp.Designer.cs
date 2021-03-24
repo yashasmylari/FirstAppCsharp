dcfbf38e15509a1edef451fd1d53f041439453b6
YASHAS <yashasmylari93@gmail.com>
2021-03-24 17:35:29 +0100

    Adding images to Application

--- FirstAppCsharp/FrmFirstApp.Designer.cs
+++ FirstAppCsharp/FrmFirstApp.Designer.cs
@@ -29,13 +29,77 @@ namespace FirstAppCsharp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmFirstApp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PicOne = new System.Windows.Forms.PictureBox();
+            this.PicTwo = new System.Windows.Forms.PictureBox();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PicOne)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicTwo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bernard MT Condensed", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(211, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to My first Program ";
+            // 
+            // PicOne
+            // 
+            this.PicOne.Image = ((System.Drawing.Image)(resources.GetObject("PicOne.Image")));
+            this.PicOne.Location = new System.Drawing.Point(12, 118);
+            this.PicOne.Name = "PicOne";
+            this.PicOne.Size = new System.Drawing.Size(382, 260);
+            this.PicOne.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicOne.TabIndex = 1;
+            this.PicOne.TabStop = false;
+            // 
+            // PicTwo
+            // 
+            this.PicTwo.Image = global::FirstAppCsharp.Properties.Resources._38d7957be5c7f4372b0185d2490aefea;
+            this.PicTwo.Location = new System.Drawing.Point(406, 118);
+            this.PicTwo.Name = "PicTwo";
+            this.PicTwo.Size = new System.Drawing.Size(382, 260);
+            this.PicTwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicTwo.TabIndex = 2;
+            this.PicTwo.TabStop = false;
+            // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(301, 89);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(204, 23);
+            this.TxtName.TabIndex = 3;
+            // 
+            // FrmFirstApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.PicTwo);
+            this.Controls.Add(this.PicOne);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmFirstApp";
+            this.Text = "FirstApp";
+            ((System.ComponentModel.ISupportInitialize)(this.PicOne)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicTwo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox PicOne;
+        private System.Windows.Forms.PictureBox PicTwo;
+        private System.Windows.Forms.TextBox TxtName;
     }
 }
 
